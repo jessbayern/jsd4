8ad726316da3ab70817fefbc5ee776d6e81c10e6
jessbayern <jessbayern@jessbayern-macbookair.roam.corp.google.com>
2016-09-26 07:41:41 -0700

    homework 1

--- homework/formulas/js/formulas.js
+++ homework/formulas/js/formulas.js
@@ -8,25 +8,35 @@
 
 // defining temperature conversion functions
 function calcFahrenheitToCelcius (f) {
-	var c= (f - 32) * 5/9
-	return c
-}
+	var c= (f - 32) * 5/9;
+	return c;
+};
 
 function calcCelciusToFarenheit (c) {
-	var f=c * (9/5) + 32
-	return f
-}
+	var f=c * (9/5) + 32;
+	return f;
+};
 
 // defining circumference calculation function
 function calcCircumference (radius){
-	var circumf= 2 * Math.PI * radius
-	return circumf
-}
+	// calculate circumference if radius >0
+	if (radius>0){
+		var circumf= 2 * Math.PI * radius;
+		return circumf;
+	};
+	// if radius <=0, do not calculate
+	return "";
+};
 
 // defining pythagorean theorem function
 function calcLongestSide(a,b){
-	var c=Math.sqrt((a*a)+(b*b))
-	return c
-}
+	// calculate longest side if both sides >0
+	if (a>0 && b>0){
+		c=Math.sqrt((a*a)+(b*b));
+		return c;
+	};
+	// if one or both sides <= 0, do not calculate
+	return "";	
+};
 
 

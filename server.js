4ee90d81f6332917ab2134608af7e7f0a1251aec
jessbayern <jessbayern@jessbayern-macbookair.roam.corp.google.com>
2016-11-07 21:17:59 -0800

    node server shiz

--- server.js
+++ server.js
@@ -8,6 +8,13 @@ var router = express.Router();
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views')
+
+// routes
+router.get('/', function(req, res) {
+    res.render('home');
+});
 app.use('/', router);
 
 
